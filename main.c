704a6c16df7d4d6cfb833558cc493c7b84d69420
MtThoas <matthias.pecquery@gmail.com>
2022-12-07 16:48:54 +0100

    initialisation projet

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <termios.h>          
+#include <unistd.h>     
+#include <stdlib.h>
+#include <sys/select.h>
+#include <stdbool.h>
+#include <fcntl.h>
+
+#include "menu.h"	
+
+#define KRED  "\x1B[31m"
+#define KNRM  "\x1B[0m"
+
+
+int main(int argc, char * argv[]) {
+
+	(void) argc;
+	(void) argv;
+	
+	menuGame();
+
+	return 0;
+}
